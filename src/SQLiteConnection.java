84498be792ece6f2e520ea3a5a8b42a938627f92
Makvi-droid <pasbenauro05@gmail.com>
2025-04-30 18:19:56 +0800

    added code to connect to student.sqlite database

--- src/SQLiteConnection.java
+++ src/SQLiteConnection.java
@@ -1,3 +1,8 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +13,34 @@
  * @author ADMIN
  */
 public class SQLiteConnection {
+     private static final String database = "jdbc:sqlite:.//database//student.sqlite";
+    private static Connection connection;
     
+    public static Connection getConnection(){
+        if(connection == null){
+            try{
+                Class.forName("org.sqlite.JDBC");
+                connection = DriverManager.getConnection(database);
+                System.out.println("Connected to database");
+            }catch(ClassNotFoundException | SQLException e){
+                e.printStackTrace();
+         }
+            
+    }
+        return connection;
+    }
+    
+    public static void closeConnection(){
+        if(connection != null){
+            
+            try{
+                connection.close();
+                System.out.println("Disconnected from database");
+            }
+            catch(SQLException e){
+                e.printStackTrace();
+            }
+            
+        }
+    }
 }
